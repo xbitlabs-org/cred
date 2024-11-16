43f64c7a896eafeb2f9ef08d53ceba2f0b1fc1a8
xbitlabs-org <sagar.xbitlabs@gmail.com>
2024-11-16 16:15:06 +0530

    added ref files

---
+++ reference_files/sample_certificate.js
@@ -0,0 +1,22 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "sample_name";
+
+// Set the program name
+document.getElementById("programName").textContent = "sample_program_name";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "sample_date";
+
+// set the certificationId
+document.getElementById("certificationId").textContent = "sample_cert_id";
+// Set the achievement text
+document.getElementById("achievementText").textContent = "sample_achievement_text"
+// Set the partner name text
+document.getElementById("partnerName").textContent = "XBL University"
+
+document.getElementById("overallScore").textContent = "overall-Score";
+
+document.getElementById("signatoryName").textContent = "https://www.xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs IN";
